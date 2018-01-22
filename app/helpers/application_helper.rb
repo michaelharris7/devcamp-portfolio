7b2e2600f8601b3b5147b759aee302631e42e06e
Michael Harris <michaeljh7@gmail.com>
2018-01-22 07:36:05 -0700

    Refactored alerts into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,4 +59,19 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, :title => "Michael Harris Portfolio Notification", sticky: false)
+  end
 end
+
+
+
+
