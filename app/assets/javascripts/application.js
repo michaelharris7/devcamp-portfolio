dca14d006f4b8c56f78272b9c2d28d2b205b17bd
Michael Harris <michaeljh7@gmail.com>
2018-03-26 12:32:10 -0600

    Updated application.js with functionality to ping heroku server so it doesn't sleep as a workaround for new relic moving their alerts to a paid account

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,8 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+
+var http = require("http");
+setInterval(function() {
+    http.get("http://www.harriscoding.com/");
+}, 300000); // every 5 minutes (300000)
