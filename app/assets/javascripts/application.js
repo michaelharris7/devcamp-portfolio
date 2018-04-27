667914d68e6073ec307ca54c72f33ffe03b102ed
Michael Harris <michaeljh7@gmail.com>
2018-04-27 15:09:44 -0600

    Took out stay alive script as it wasn't working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,8 +21,3 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
-
-var http = require("http");
-setInterval(function() {
-    http.get("http://www.harriscoding.com/");
-}, 300000); // every 5 minutes (300000)
