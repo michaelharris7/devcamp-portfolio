73ee59f03da0295d2b59742bd65cc03963f12c90
Michael Harris <michaeljh7@gmail.com>
2019-03-14 17:18:14 -0600

    Updated integration production calls for action cable with custom url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://mh-devcamp-portfolio.herokuapp.com', 'http://mh-devcamp-portfolio.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['https://mh-devcamp-portfolio.herokuapp.com', 'http://mh-devcamp-portfolio.herokuapp.com', 'http://www.michaelharrisportfolio.com', 'https://www.michaelharrisportfolio.com/']
   config.action_cable.url = "wss://mh-devcamp-portfolio.herokuapp.com/cable"
 end
