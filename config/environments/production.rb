c4136e8b61dc08571958d4db333f677765d808c3
Michael Harris <michaeljh7@gmail.com>
2019-03-14 18:32:09 -0600

    Updated action cable request origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_cable.allowed_request_origins = ['https://mh-devcamp-portfolio.herokuapp.com', 'http://mh-devcamp-portfolio.herokuapp.com', 'http://www.michaelharrisportfolio.com', 'https://www.michaelharrisportfolio.com/']
+  # config.action_cable.allowed_request_origins = ['https://mh-devcamp-portfolio.herokuapp.com', 'http://mh-devcamp-portfolio.herokuapp.com', 'http://www.michaelharrisportfolio.com', 'https://www.michaelharrisportfolio.com/']
+  config.action_cable.allowed_request_origins = ['http://www.michaelharrisportfolio.com', 'https://www.michaelharrisportfolio.com/']
   config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
 end
